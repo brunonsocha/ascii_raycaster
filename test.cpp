265ea91ae94247908b474009843cbff159661507
brunonsocha <brunon.socha@gmail.com>
2026-01-26 18:25:50 +0100

    movement works on entering letter and confirming

--- test.cpp
+++ test.cpp
@@ -1,3 +1,5 @@
+#include <cmath>
+#include <vector>
 #include <iostream>
 
 enum Tile {
@@ -9,19 +11,16 @@ class Map {
     public:
         int width;
         int height;
-        Map(int x, int y) {
-            width = x;
-            height = y;
-        }
-        Tile Grid[width*height];
+        Map(int x, int y) : width(x), height(y), grid(x * y, Empty) {}
+        std::vector<Tile> grid;
         Tile getTile(int gx, int gy) {
-            return Grid[gy*width + gx];
+            return grid[gy*width + gx];
         }
         bool isWall(int gx, int gy) {
-            if (getTile(gx, gy) == Wall || gx >= width || gx < 0 || gy >= height || gy < 0) {
+            if (gx >= width || gx < 0 || gy >= height || gy < 0) {
                 return true;
             } 
-            return false;
+            return getTile(gx, gy) == Wall;
         }
 };
 
@@ -44,8 +43,12 @@ class Pickup : public Entity {
 
 class Player : public Entity {
     public:
+        Player(double x, double y) {
+            X = x;
+            Y = y;
+        }
         void Move(double xa, double ya, Map* map) {
-            if (map.isWall(X + xa, Y + ya) {
+            if (map->isWall(std::floor(X+xa), std::floor(Y+ya))) {
                 return;
             } else {
                 X += xa;
@@ -54,19 +57,81 @@ class Player : public Entity {
         }
 };
 
-
-void drawMap(int x, int y);
+void clearScreen();
+void drawTopDownMap(Map &map, Player &player);
 
 int main() {
-    drawMap(5, 5);
+    const char* layout[] = {
+        "####################",
+        "#........#.........#",
+        "#........#.........#",
+        "#........#.........#",
+        "#........#.........#",
+        "#........#.........#",
+        "#..................#",
+        "#..................#",
+        "#..................#",
+        "####################"
+    };
+
+    Map map(20, 10);
+    Player player(3.5, 4.8);
+
+    for (int y = 0; y < map.height; y++) {
+        for (int x = 0; x < map.width; x++) {
+            map.grid[y * map.width + x] =
+                (layout[y][x] == '#') ? Wall : Empty;
+        }
+    }
+    const double step = 0.5;
+    for (;;) {
+        clearScreen();
+        drawTopDownMap(map, player);
+        char c;
+        std::cin >> c;
+        switch (c) {
+            case 'q':
+                return 0;
+            case 'w':
+                player.Move(0, -step, &map);            
+                break;
+            case 's':
+                player.Move(0, step, &map);            
+                break;
+            case 'd':
+                player.Move(step, 0, &map);            
+                break;
+            case 'a':
+                player.Move(-step, 0, &map);            
+                break;
+            default:
+                break;
+            }
+    }
     return 0;
 }
 
-void drawMap(int x, int y) {
-    for (int i = 0; i < y; i++) {
-        for (int j = 0; j < x; j++) {
-            std::cout << "#";
+void drawTopDownMap(Map &map, Player &player) {
+    int pX = std::floor(player.getXPos());
+    int pY = std::floor(player.getYPos());
+    for (int i = 0; i < map.height; i++) {
+        for (int j = 0; j < map.width; j++) {
+            if (pX == j && pY == i) {
+                std::cout << "P";
+            } 
+            else {
+                if (map.isWall(j, i)) {
+                    std::cout << "#";
+                } 
+                else {
+                    std::cout << ".";
+                }
+            }
         }
         std::cout << "\n";
     }
+}
+
+void clearScreen() {
+    std::cout << "\x1B[2J\x1B[H";
 }
